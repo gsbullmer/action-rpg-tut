158d61ac03a1542d9b0a4e1d87288ad430f2ed5d
Greg Bullmer <gsbullmer@gmail.com>
2020-06-16 15:48:18 -0500

    Finish pt. 14: Export variables and setget

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 
 var knockback = Vector2.ZERO
 
+onready var stats = $Stats
+
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -10,4 +12,9 @@ func _physics_process(delta):
 
 
 func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 125
+
+
+func _on_Stats_no_health():
+	queue_free()
