173f815cfbd85e792e99a7273a65ec8f19021d5e
Greg Bullmer <gsbullmer@gmail.com>
2020-06-16 17:01:55 -0500

    Finish pt. 16: Bat chase AI

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,16 +1,52 @@
 extends KinematicBody2D
 
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
 var knockback = Vector2.ZERO
+var state = IDLE
+var velocity = Vector2.ZERO
 
 onready var stats = $Stats
+onready var player_detection_zone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
 
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			
+		WANDER:
+			pass
+			
+		CHASE:
+			var player = player_detection_zone.player
+			if player != null:
+				var direction = Vector2(player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	
+	sprite.flip_h = velocity.x < 0
+	velocity = move_and_slide(velocity)
+
+
+func seek_player():
+	if player_detection_zone.can_see_player():
+		state = CHASE
 
 
 func _on_Hurtbox_area_entered(area):
